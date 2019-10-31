a50203eb84db19d6d7dc5b0d685cc5d0a75df2b4
laetitia <laetitiaconstantin1998@gmail.com>
2019-10-31 15:10:58 +0100

    test widget title and description

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,22 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:edelsten/views/common/common.dart';
 
 import 'package:edelsten/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('My widget has a title and a description', (WidgetTester tester) async {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Widget to test
+    await tester.pumpWidget(TitleAndDescription(title: 'T', content: 'D'));
+    
+    // Search for widget using finder
+    final titleFinder = find.text('T');
+    final contentFinder = find.text('D');
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify the widget using a Matcher
+    expect(titleFinder, findsOneWidget);
+    expect(contentFinder, findsOneWidget);
   });
 }
