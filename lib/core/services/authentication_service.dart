bef7f28903dc421c3cec7096578558ce2a47a89c
THERY Adrien <kikuiredkill@gmail.com>
2019-11-01 23:25:34 +0100

    clean authen service

--- lib/core/services/authentication_service.dart
+++ lib/core/services/authentication_service.dart
@@ -4,6 +4,7 @@ import 'dart:core';
 import 'package:edelsten/core/models/model.dart';
 import 'package:edelsten/core/repositories/user_repository.dart';
 import 'package:edelsten/locator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenticationService  {
   
@@ -12,18 +13,15 @@ class AuthenticationService  {
 
   Future<bool> login (String identifier, String password) async {
     
-    var firebaseUser;
+    FirebaseUser firebaseUser;
     firebaseUser = await _userRepository.loginUser(email: identifier, password: password);
     User userData;
     
     if (firebaseUser != null){
-      userData = await _userRepository.getUserData(/*firebaseUser.uid*/"JzBpHeHWtqAFazpj5v0o");
-      print('-------------------------------------------------------------userData');
-      print(userData);
+      userData = await _userRepository.getUserData(firebaseUser.uid);
     }
     var hasUser = userData != null;
     if (hasUser) {
-      print('-------------------------------------------------------------hasUserOK');
       userController.add(userData);
     }
     return hasUser;
