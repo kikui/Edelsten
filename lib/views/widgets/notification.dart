6397306ec46baddfe4343655e5792adbe6e30436
Arnold DOMAYA <domaya_arnold@yahoo.fr>
2019-11-07 15:43:26 +0100

    delete old notification widget class

--- lib/views/widgets/notification.dart
+++ lib/views/widgets/notification.dart
@@ -1,36 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flushbar/flushbar.dart';
 
-class NotificationWidget extends StatelessWidget {
-  const NotificationWidget(
-      {@required this.message,
-      @required this.isSucced,
-      @required this.title,
-      @required this.context});
-
-  final String message;
-  final bool isSucced;
-  final String title;
-  final BuildContext context;
-
-  @override
-  Widget build(BuildContext context) {
-    Color color = Colors.red[200];
-    if (isSucced) 
-    { 
-      color = Colors.green[300];
-    } 
-
-    return Flushbar(
-      title: title,
-      message: message,
-      flushbarPosition: FlushbarPosition.TOP,
-      backgroundColor: color,
-      duration: Duration(seconds: 4),
-    )..show(context);
-  }
-}
-
 Widget getNotificationWidget({message, isSucced, title, context}){
   Color color = Colors.red[200];
     if (isSucced) 
